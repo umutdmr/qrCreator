9e613a1fe93ec81d4a8ca396386181287c31576f
Umut Demir <umutdmr315@gmail.com>
2021-05-16 17:51:45 +0300

    create qr codes and save to the created_qr_codes folder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pyqrcode as qr
+import os
 
 url = input("Enter the url:\n")
 qr_url = qr.create(url)
-qr_url.png("result.png", scale=10)
+url = "_".join(url.split("https://")[1].split("/"))
+os.chdir(os.getcwd() + "\created_qr_codes")
+qr_url.png(url + ".png", scale=10)
