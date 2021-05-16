7fd9224f84c36aaf278847f1ee8137d49992a7e9
Umut Demir <umutdmr315@gmail.com>
2021-05-16 18:02:36 +0300

    added operating_system variable. if os is not ms, then program use '/' for changing the directory.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pyqrcode as qr
 import os
 
+operating_system = os.name
 url = input("Enter the url:\n")
 qr_url = qr.create(url)
 url = "_".join(url.split("https://")[1].split("/"))
-os.chdir(os.getcwd() + "\created_qr_codes")
+if operating_system == "nt":
+    os.chdir(os.getcwd() + "\created_qr_codes")
+elif operating_system == "posix":
+    os.chdir(os.getcwd() + "/created_qr_codes")
 qr_url.png(url + ".png", scale=10)
